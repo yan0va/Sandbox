36c286f457265ffef9320a9773fd96095ea62175
Anna <anna.yanova@gmail.com>
2016-04-07 14:47:21 +0100

    Asking user to input some characters

--- Ruby/countingCharacters.rb
+++ Ruby/countingCharacters.rb
@@ -1,4 +1,13 @@
+#Challenge: If the user enters nothing, state that the user must enter something into the program.
+
 puts "What is the input string?"	
-input = gets.chomp
-counting = input.length
-puts "#{input} has #{counting} characters."
+
+while true
+    input = gets.chomp
+	if !input.empty?
+		counting = input.length
+    	puts "#{input} has #{counting} characters."
+    	break
+    end
+    puts "Input has no characters. Please enter any characters."
+end
